405719cbe78cbef958575f6166dbc825625f1a56
蔡祐臻 <kellytsai@airbears2-10-142-49-152.airbears2.1918.berkeley.edu>
2019-10-13 20:34:01 -0700

    Updates

---
+++ precache-manifest.0c187f630b6e1185a56b7a213600fa73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d51304a4b0518a02b68ecc90885fbf",
+    "url": "/API_project/index.html"
+  },
+  {
+    "revision": "5668c81f2cf042b78dbb",
+    "url": "/API_project/static/css/main.716a20c7.chunk.css"
+  },
+  {
+    "revision": "8ae4de82265a75ac7413",
+    "url": "/API_project/static/js/2.3d6e387b.chunk.js"
+  },
+  {
+    "revision": "5668c81f2cf042b78dbb",
+    "url": "/API_project/static/js/main.68537fd9.chunk.js"
+  },
+  {
+    "revision": "d8a3dbf40c81f0b22f72",
+    "url": "/API_project/static/js/runtime-main.84b030cc.js"
+  },
+  {
+    "revision": "eff1ffedf3415f54e92cd7c1c1319f41",
+    "url": "/API_project/static/media/cat.eff1ffed.png"
+  }
+]);
